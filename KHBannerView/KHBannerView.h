2d478376520a1d09b6990c62ff69f778d010eed6
KyeongHun Kim <kimkhuna1@gmail.com>
2024-12-15 22:33:34 +0900

    최초 커밋

---
+++ KHBannerView/KHBannerView.h
@@ -0,0 +1,18 @@
+//
+//  KHBannerView.h
+//  KHBannerView
+//
+//  Created by 김경훈 on 12/15/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KHBannerView.
+FOUNDATION_EXPORT double KHBannerViewVersionNumber;
+
+//! Project version string for KHBannerView.
+FOUNDATION_EXPORT const unsigned char KHBannerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KHBannerView/PublicHeader.h>
+
+
